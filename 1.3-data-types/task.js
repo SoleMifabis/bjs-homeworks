521394a9018c7880ea728c9cd11aeadc80417ee1
SoleMifabis <sofia.bilmes@gmail.com>
2021-04-20 15:50:01 +0500

    first commit data types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (typeof percent !== "number") {
+        return console.log(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
+    };
+    if (typeof contribution !== "number") {
+        return console.log(`Параметр "начальный взнос" содержит неправильное значение ${contribution}`);
+    };
+    if (typeof amount !== "number") {
+        return console.log(`Параметр "общая стоимость" содержит неправильное значение ${amount}`);
+    };
+
+    let body = amount - contribution;
+
+    let monthsNumber = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
+    let P = (percent/100)/12;
+    let amountPerMonth = body * (P + P / (((1 + P) ** monthsNumber) - 1));
+    let totalAmount = amountPerMonth * monthsNumber;
+    return totalAmount.toFixed(1);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    
+    if (name.length > 0)
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    if (name.length <= 0 || undefined) 
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+    
+    return greeting;
 }
+
+
