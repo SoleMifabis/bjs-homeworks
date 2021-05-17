2334b7cd8c50da6ad745d156fe0cb1bb0fa2a596
SoleMifabis <sofia.bilmes@gmail.com>
2021-05-17 22:06:44 +0500

    Second commint

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,14 +1,17 @@
 'use strict';
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    if (typeof percent !== "number") {
+    if (typeof percent !== "number" && percent < 0) {
         return console.log(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
     };
-    if (typeof contribution !== "number") {
+    if (typeof contribution !== "number" && contribution < 0) {
         return console.log(`Параметр "начальный взнос" содержит неправильное значение ${contribution}`);
     };
-    if (typeof amount !== "number") {
+    if (typeof amount !== "number" && amount < 0) {
         return console.log(`Параметр "общая стоимость" содержит неправильное значение ${amount}`);
+    };
+    if (data < new Date()) {
+        return console.log(`Параметр "дата" содержит неправильное значение ${data}`);
     };
 
     let body = amount - contribution;
@@ -17,18 +20,12 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let P = (percent/100)/12;
     let amountPerMonth = body * (P + P / (((1 + P) ** monthsNumber) - 1));
     let totalAmount = amountPerMonth * monthsNumber;
-    return totalAmount.toFixed(1);
+    
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    let greeting;
-    
-    if (name.length > 0)
-        greeting = `Привет, мир! Меня зовут ${name}.`;
-    if (name.length <= 0 || undefined) 
-        greeting = `Привет, мир! Меня зовут Аноним.`;
-    
-    return greeting;
+    return `Привет, мир! Меня зовут ${name && name.length > 0 ? name : 'Аноним'}.`;
 }
 
 
